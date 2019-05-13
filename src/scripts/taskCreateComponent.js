2833dd191673cb9b4bfd670e50342192be782109
Emily Loggins <em.loggins7@gmail.com>
2019-05-13 10:07:32 -0500

    changed files around

--- src/scripts/taskCreateComponent.js
+++
@@ -1,9 +0,0 @@
-/*
-    Author: Emily
-    Name: taskCreateComponent
-    Purpose: Displaying the "New Task" button, and attaching an event listener that presents the "New Task" form when clicked.
-*/
-
-
-
-
