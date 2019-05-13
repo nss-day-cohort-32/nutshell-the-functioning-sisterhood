f0ee065da0f337161bbb727408bbfa88a00e6a9e
Shelley Arnold <shelle.kay@gmail.com>
2019-05-13 11:52:25 -0500

    friends list setup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,16 @@
 import welcomeMessage from "./users-landing"
 import userFeed from "./users-feed.js"
-import usersAPImanager from "./users-apiManager.js"
 import loginMethods from "./users-login"
 import usersLogOut from "./users-logOut"
 
+import userList from "./friends-list"
 
+userList.printToDOM()
 
-if(loginMethods.getLoggedInUser() !== null) {
-    userFeed.printToDOM()
-    usersLogOut();
-}else {
-    welcomeMessage.printToDOM()
-    document.querySelector(".logout-button-main").style.display="none"
-}
+// if(loginMethods.getLoggedInUser() !== null) {
+//     userFeed.printToDOM()
+//     usersLogOut();
+// }else {
+//     welcomeMessage.printToDOM()
+//     document.querySelector(".logout-button-main").style.display="none"
+// }
